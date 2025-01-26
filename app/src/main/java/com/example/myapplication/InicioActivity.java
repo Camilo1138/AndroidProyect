c823eff881857d0013c74ab7277c31dd53e34f38
Camilo1138 <camiarbo@espol.edu.ec>
2025-01-26 09:01:18 -0500

    cambios

--- app/src/main/java/com/example/myapplication/InicioActivity.java
+++ app/src/main/java/com/example/myapplication/InicioActivity.java
@@ -1,20 +1,29 @@
 package com.example.myapplication;
+
+
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class InicioActivity extends AppCompatActivity {
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class InicioActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private ListView eventListView;
+
 
     private boolean isFabOpen = false;
     private TextView tvProfileName;
@@ -24,12 +33,15 @@ public class InicioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.inicio), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        initWidgets();
+        // Asegúrate de que selectedDate está inicializado
+        // Inicializa la fecha seleccionada
+        if (CalendarUtils.selectedDate == null) {
+            CalendarUtils.selectedDate = LocalDate.now();
+        }
+
+        setWeekView();
         // Vincula el TextView
         tvProfileName = findViewById(R.id.profile_name);
 
@@ -41,4 +53,54 @@ public class InicioActivity extends AppCompatActivity {
         }
 
     }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+        eventListView = findViewById(R.id.eventListView);
+    }
+
+    private void setWeekView() {
+        monthYearText.setText(CalendarUtils.monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = CalendarUtils.daysInWeekArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+        setEventAdapter();
+    }
+
+    public void previousWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
+    }
+
+    public void nextWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+        CalendarUtils.selectedDate = date;
+        setWeekView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setEventAdapter();
+    }
+
+    private void setEventAdapter() {
+        ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.selectedDate);
+        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEvents);
+        eventListView.setAdapter(eventAdapter);
+    }
+
+    public void newEventAction(View view) {
+        startActivity(new Intent(this, EventEditActivity.class));
+    }
+
 }
